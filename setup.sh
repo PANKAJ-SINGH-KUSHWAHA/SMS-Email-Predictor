4abd3fae70838937587b2447e0e8b728f461bafc
PANKAJ SINGH KUSHWAHA <145460446+PANKAJ-SINGH-KUSHWAHA@users.noreply.github.com>
2025-02-16 15:55:22 +0530

    updated setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,12 @@
+mkdir -p ~/.streamlit/
+
+echo "\
+[server]\n\
+port = $PORT\n\
+enableCORS = false\n\
+headless = true\n\
+\n\
+" > ~/.streamlit/config.toml
 !python -c "import nltk; nltk.download('punkt')"
 !python -c "import nltk; nltk.download('stopwords')"
 !python -c "import nltk; nltk.download('wordnet')"
